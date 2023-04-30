85d8cd265624920ab0cbc796acf1a86cb07cb16f
LinRaymond2006 <53900158+LinRaymond2006@users.noreply.github.com>
2023-05-01 07:16:04 +0800

    rewrite the fault handler later

--- kernel/main.c
+++ kernel/main.c
@@ -3,7 +3,10 @@
 #include "stdlib.h"
 #include "string.h"
 void Start_Kernel(void) {
+    intit_sysirq();
     printf("kernel started, function %s running\n", __FUNCTION__);
+    int i=1;
+    int b=i/0;
     while (1) {
         ;
     }
